256a0cec142b0d95b724b2e3baf69778263fb139
Brayden Curtis <braydencurtis@gmail.com>
2020-03-12 13:53:00 -0700

    For Loop that logs every array entry

---
+++ snarkyFem_for.js
@@ -0,0 +1,6 @@
+var snarkyFeminists = ["Janeane Garofalo", "Lisa Simpson", "Rachel Maddow", "Margaret Cho", "Amy Poehler", "Leela (from Futurama)", "Daria Morgendorffer (from Daria)", "Maureen Dowd", "Wanda Sykes", "Roseanne Barr"];
+
+for (var i = 0; i < snarkyFeminists.length; i++) {
+    var snarkyFeminist = snarkyFeminists[i];
+    console.log(snarkyFeminists[i]);
+}
