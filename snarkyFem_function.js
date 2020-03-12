44f5b8c696f277c2675ddc02bc33e969b31ddb94
Brayden Curtis <braydencurtis@gmail.com>
2020-03-12 13:53:29 -0700

    Turned snarkyFeminists into a callable function

---
+++ snarkyFem_function.js
@@ -0,0 +1,11 @@
+var snarkyFeminists = ["Janeane Garofalo", "Lisa Simpson", "Rachel Maddow", "Margaret Cho", "Amy Poehler", "Leela (from Futurama)", "Daria Morgendorffer (from Daria)", "Maureen Dowd", "Wanda Sykes", "Roseanne Barr"];
+
+var listFeminists = function () {
+
+    for (var i = 0; i < snarkyFeminists.length; i++) {
+        var snarkyFeminist = snarkyFeminists[i];
+        console.log(snarkyFeminists[i]);
+    }
+};
+
+listFeminists();
