242c611badb9a94744959487bbee8a797164bf0e
Brayden Curtis <braydencurtis@gmail.com>
2020-03-12 13:23:51 -0700

    Initial commit

---
+++ dalmation_for.js
@@ -0,0 +1,6 @@
+
+for (var safeDalmations = 2; safeDalmations < 101; safeDalmations++) {
+    console.log("There are " + safeDalmations  + " dalmations safe! Time to rescue another one!")
+};
+
+console.log("Hooray! All " + safeDalmations + " dalmations are safe!");
---
+++ dalmation_while.js
@@ -0,0 +1,8 @@
+var safeDalmations = 2;
+
+while (safeDalmations < 101) {
+    console.log("There are " + safeDalmations + " dalmations safe! Time to rescue another one!");
+    safeDalmations++;
+};
+
+console.log("Hooray! All " + safeDalmations + " dalmations are safe! Roll the credits!");
